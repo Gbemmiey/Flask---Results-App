eb32cd0d2d2fd668e68c75ed2ba21c2b9a284ca1
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2023-01-11 22:42:40 +0000

    Update: Added support for system env variables

--- config.py
+++ config.py
@@ -2,12 +2,18 @@ import os
 
 from dotenv import dotenv_values
 
-env_variables = dotenv_values()
+try:
+    env_variables = dotenv_values()
+    database_name = env_variables['DATABASE_NAME']
+    username = env_variables['DATABASE_USERNAME']
+    password = env_variables['DATABASE_PASSWORD']
+    database_uri = env_variables['DATABASE_URI']
+except:
+    database_name = os.environ['DATABASE_NAME']
+    username = os.environ['DATABASE_USERNAME']
+    password = os.environ['DATABASE_PASSWORD']
+    database_uri = os.environ['DATABASE_URI']
 
-database_name = env_variables['DATABASE_NAME']
-username = env_variables['DATABASE_USERNAME']
-password = env_variables['DATABASE_PASSWORD']
-database_uri = env_variables['DATABASE_URI']
 
 database_path = f'postgresql://{username}:{password}@{database_uri}/{database_name}'
 SQLALCHEMY_DATABASE_URI = database_path
