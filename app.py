c835a0df49a20f14bb042517a4acd8ff4c791e38
Oluwagbemileke Femi Oyeyoade <gbemfemalabi@gmail.com>
2023-02-15 09:56:04 +0000

    Update: unknown

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-import json
-from flask import Flask, render_template, request, Response, flash, redirect, url_for, jsonify
+"""Dynamic Web Application for Results App"""
+
+from flask import Flask, render_template, flash, redirect, jsonify
 from flask_moment import Moment
 from flask_migrate import Migrate
 
+from flask import jsonify, make_response, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+import uuid
+import jwt
+import datetime
+
 from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
-
 
 
 app = Flask(__name__)
@@ -31,6 +38,30 @@ class Staff(db.Model):
         self.role = role
 
 
+class Class(db.Model):
+    """Model created for Students"""
+    __table__name = 'class'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    class_teacher_id = Column(Integer)
+
+
+class Subject(db.Model):
+    """Model created for Students"""
+    __table__name = 'subject'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+
+class Student(db.Model):
+    """Model created for Students"""
+    __table__name = 'student'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    access_code = Column(String)
+    class_id = Column(Integer)
+
+
 @app.route("/")
 def hello():
     """Return Hello"""
@@ -42,15 +73,19 @@ def display_login():
     """Displays login page"""
     return render_template('forms/login.html')
 
+
 @app.route("/signup")
 def display_signup():
     """Displays login page"""
     return render_template('forms/signup.html')
 
+
 @app.route('/auth', methods=['POST'])
 def authenticate_user():
+    """Authenticates users"""
     flash("Logged In!")
-    return hello()
+    return redirect('/')
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -62,6 +97,7 @@ def not_found(error):
         'error': 404,
         'message': 'Not Found'
     }), 404
+
 
 @app.errorhandler(422)
 def unprocessable_entity(error):
@@ -76,4 +112,4 @@ def unprocessable_entity(error):
 
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
